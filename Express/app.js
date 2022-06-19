1a07d8246284e9ff013676d77f2821e4a08d93d4
Hishan Shrestha <hishanshrestha29@gmail.com>
2022-06-19 14:21:19 +0545

    Express

--- Express/app.js
+++ Express/app.js
@@ -1,31 +1,29 @@
-const Joi=require('joi');
-const logger=require('../Middleware/logger');
-const authenticating=require('../Middleware/authenticating');
-const debug=require('debug')('app:startup');
+const Joi = require("joi");
+const logger = require("../Middleware/logger");
+const authenticating = require("../Middleware/authenticating");
+const debug = require("debug")("app:startup");
 
-const config=require('config');
-const morgan=require('morgan');
-const helmet=require('helmet');
-const express= require('express');
-const app= express();
-const courses=require('./routes/courses');
-const home=require('./routes/home');
-app.use('/',home);
+const config = require("config");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const express = require("express");
+const app = express();
+const courses = require("./routes/courses");
+const home = require("./routes/home");
+app.use("/", home);
 
-app.set('view engine','pug');
-app.set('views','./views'); //default
-
+app.set("view engine", "pug");
+app.set("views", "./views"); //default
 
 //Environment
 // console.log(`NODE_ENV:${process.env.NODE_ENV}`);
 // console.log(`app:${app.get('env')}`);
 
-
 //Built in middleware function
-app.use(express.json());//adding a piece of middleware //parsing of json object
-app.use(express.urlencoded({ extended:true })); //parse incoming req with urlencoded  payloads
-app.use(express.static('public'));//serves static assets such as html,css files
-app.use('/api/courses',courses);
+app.use(express.json()); //adding a piece of middleware //parsing of json object
+app.use(express.urlencoded({ extended: true })); //parse incoming req with urlencoded  payloads
+app.use(express.static("public")); //serves static assets such as html,css files
+app.use("/api/courses", courses);
 
 //created middleware function
 app.use(logger);
@@ -33,25 +31,22 @@ app.use(authenticating);
 
 //third party middleware
 app.use(helmet());
-//app.use(morgan('tiny')); 
-
+//app.use(morgan('tiny'));
 
 //Configuration
-console.log('Application Name: '+config.get('name'));
-console.log('Mail Server: '+config.get('mail.host'));
-console.log('Mail Password: '+config.get('mail.password'));
+console.log("Application Name: " + config.get("name"));
+console.log("Mail Server: " + config.get("mail.host"));
+console.log("Mail Password: " + config.get("mail.password"));
 
-
-if (app.get('env')=='development') {
-    app.use(morgan('tiny'));
-    debug('Morgan enabled...');
+if (app.get("env") == "development") {
+  app.use(morgan("tiny"));
+  debug("Morgan enabled...");
 }
 //DB work
 // dbDebugger('Connected to database');
 
+const port = process.env.PORT || 3000;
 
-const port=process.env.PORT ||3000;
-
-app.listen(port, ()=>{
-    console.log(`Listening on port ${port}... `)
+app.listen(port, () => {
+  console.log(`Listening on port ${port}... `);
 });
--- Express/routes/home.js
+++ Express/routes/home.js
@@ -1,8 +1,8 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('index',{title:'My express App',message:'Hello'});
+router.get("/", (req, res) => {
+  res.render("index", { title: "My express App", message: "Hello" });
 });
 
-module.exports=router;
+module.exports = router;
