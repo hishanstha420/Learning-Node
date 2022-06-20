db32f8e3fd334a4dd7d6f7c056f24989f41ea2eb
Hishan Shrestha <hishanshrestha29@gmail.com>
2022-06-20 15:36:24 +0545

    index

--- Vidly-mongodb/index.js
+++ Vidly-mongodb/index.js
@@ -1,9 +1,12 @@
-const mongoose = require("mongoose");
 const Joi = require("joi");
+Joi.objectId = require("Joi-objectid")(Joi);
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const genres = require("./routes/genre");
+const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/Vidly")
@@ -13,6 +16,8 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 
