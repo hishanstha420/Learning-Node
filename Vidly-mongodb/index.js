2e25b25f7a09be7b7ae71a7c96d6e8a8fca280e6
Hishan Shrestha <hishanshrestha29@gmail.com>
2022-06-23 16:54:37 +0545

    after authentication

--- Vidly-mongodb/index.js
+++ Vidly-mongodb/index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("Joi-objectid")(Joi);
 const mongoose = require("mongoose");
@@ -7,6 +8,13 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivatekey is not defined.");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/Vidly")
@@ -18,6 +26,8 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 
---
+++ Vidly-mongodb/routes/auth.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const mongoose = require("mongoose");
+const _ = require("lodash"); //_ by convention
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid Email or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid Email or password");
+
+  const token = user.generateAuthToken();
+  res.send(token);
+});
+
+validate = (request) => {
+  const schema = Joi.object({
+    email: Joi.string().min(3).required().email(),
+    password: Joi.string().min(8).max(255).required(),
+  });
+  return schema.validate(request);
+};
+
+module.exports = router;
