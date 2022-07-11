8ddb7c7216350f0aed85caa0e23712fad5958c7f
Hishan Shrestha <hishanshrestha29@gmail.com>
2022-07-11 07:53:16 +0545

    index

--- Vidly-mongodb/index.js
+++ Vidly-mongodb/index.js
@@ -1,34 +1,12 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("Joi-objectid")(Joi);
-const mongoose = require("mongoose");
+const winston = require("winston");
 const express = require("express");
 const app = express();
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivatekey is not defined.");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/Vidly")
-  .then(() => console.log("connection successful!!"))
-  .catch((err) => console.log("Connection Failed", err.message));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 
